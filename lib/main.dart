5a7d45b22dd2f23f314c7098cb845df2b43a676a
gowdham <gowdham.ravi@gmail.com>
2024-06-06 12:20:21 +0530

    sorted_pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
-
-const apiKey = 'AIzaSyDDVP1gTlQEEMtZOrTz2ifVrDH-mL7gbKU';
-final model = GenerativeModel(
-  model: 'gemini-1.5-flash-latest',
-  apiKey: apiKey,
-);
-ChatSession? chatBot;
-Future<String> gemini(String prompt) async {
-  final content = Content.text(prompt);
-  final response = await chatBot!.sendMessage(content);
-
-  return (response.text!);
-}
+import 'package:lorehunter/widgets/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,96 +36,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _textController = TextEditingController();
-  String chatHistory = "";
-
-  Future<void> initAI() async {
-    _textController.clear();
-    chatBot = model.startChat();
-    final response =
-        await chatBot!.sendMessage(Content.text("""You are a pirate tour guide. 
-        I will type the location that I'm in and you will generate a walking tour of that location for me. 
-        Your response will always be markdown. Your reply should be no more than 200 words"""));
-    print(response.text);
-  }
-
-  Future<void> sendMessage(String text) async {
-    _textController.clear();
-    setState(() {
-      chatHistory += "User: $text\n";
-    });
-    String response = await gemini(text);
-    setState(() {
-      chatHistory += "AI: ${response.text}\n";
-    });
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    initAI();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Chat with AI"),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.all(16),
-              child: SelectableText(
-                chatHistory,
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _textController,
-                    decoration: InputDecoration(hintText: "Type your message"),
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: () => sendMessage(_textController.text),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
