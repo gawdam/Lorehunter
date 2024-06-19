a640b4120d7a73bb45f8e6c14400ca1cb3441523
gowdham <gowdham.ravi@gmail.com>
2024-06-20 00:32:11 +0530

    Added polylines in maps

--- lib/routes/directions.dart
+++ lib/routes/directions.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MyMap extends StatefulWidget {
@@ -12,11 +14,47 @@ class MyMap extends StatefulWidget {
 
 class _MyMapState extends State<MyMap> {
   final Set<Marker> _markers = {};
+  late String _apiKey;
 
   @override
   void initState() {
     super.initState();
     _createMarkers();
+    initMaps();
+  }
+
+  void initMaps() async {
+    await dotenv.load(fileName: ".env");
+
+    final apiKey = dotenv.env['maps_api_key']!;
+  }
+
+  Future<List<LatLng>> getPolyLinePoints() async {
+    await dotenv.load(fileName: ".env");
+
+    final apiKey = dotenv.env['maps_api_key']!;
+    List<LatLng> polylineCoordinates = [];
+    PolylinePoints polylinePoints = PolylinePoints();
+    List<PolylineResult> results = [];
+    PolylineResult polylineResult;
+    for (var i = 0; i < widget.coordinates.length - 1; i++) {
+      polylineResult = await polylinePoints.getRouteBetweenCoordinates(
+          apiKey,
+          PointLatLng(
+              widget.coordinates[i].latitude, widget.coordinates[i].latitude),
+          PointLatLng(widget.coordinates[i + 1].latitude,
+              widget.coordinates[i + 1].latitude),
+          travelMode: TravelMode.walking);
+      if (polylineResult.points.isNotEmpty) {
+        polylineResult.points.forEach((PointLatLng point) {
+          polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+        });
+        results.add(polylineResult);
+      } else {
+        print(polylineResult.errorMessage);
+      }
+    }
+    return polylineCoordinates;
   }
 
   void _createMarkers() {
