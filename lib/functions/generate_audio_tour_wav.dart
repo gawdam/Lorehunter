6bfbc98bcb191d7921659da7a56012b771436856
gowdham <gowdham.ravi@gmail.com>
2024-07-31 16:44:38 +0530

    FIXED THIS FUCKIN CODE FINALLY

--- lib/functions/generate_audio_tour_wav.dart
+++ lib/functions/generate_audio_tour_wav.dart
@@ -14,37 +14,43 @@ Future<String> saveTTSAudio(String script, String type) async {
   final flutterTts = FlutterTts();
   var headerVoice;
   var contentVoice;
-  flutterTts.getVoices.then((data) {
+  await flutterTts.getVoices.then((data) {
     try {
       List<Map> voices = List<Map>.from(data);
 
       headerVoice =
-          voices.firstWhere((voice) => voice['name'] == 'en-gb-x-gbd-local');
-      contentVoice =
           voices.firstWhere((voice) => voice['name'] == 'en-gb-x-gbb-local');
+      contentVoice =
+          voices.firstWhere((voice) => voice['name'] == 'en-gb-x-gbd-local');
     } catch (e) {
       print(e);
     }
   });
+  print(headerVoice);
 
-  // Set voice, pitch, and speed based on type
-  // if (type == 'header') {
-  //   await flutterTts.setVoice(headerVoice); // Replace with desired header voice
-  //   await flutterTts.setSpeechRate(0.5); // Adjust speech rate as needed
-  //   await flutterTts.setPitch(1.0); // Adjust pitch as needed
-  // } else if (type == 'body') {
-  //   await flutterTts.setVoice(contentVoice); // Replace with desired body voice
-  //   await flutterTts.setSpeechRate(0.6); // Adjust speech rate as needed
-  //   await flutterTts.setPitch(0.9); // Adjust pitch as needed
-  // } else {
-  //   throw ArgumentError('Invalid type: $type');
-  // }
+  if (type == 'header') {
+    flutterTts.setVoice({
+      "name": headerVoice["name"],
+      "locale": headerVoice["locale"]
+    }); // Replace with desired header voice
+    flutterTts.setSpeechRate(0.3); // Adjust speech rate as needed
+    flutterTts.setPitch(1.0); // Adjust pitch as needed
+  } else if (type == 'body') {
+    flutterTts.setVoice({
+      "name": contentVoice["name"],
+      "locale": contentVoice["locale"]
+    }); // Replace with desired body voice
+    flutterTts.setSpeechRate(0.6); // Adjust speech rate as needed
+    flutterTts.setPitch(0.9); // Adjust pitch as needed
+  }
   final externalDirectory = Directory("/storage/emulated/0/Music/");
+  final internalDirectory = await getApplicationDocumentsDirectory();
 
   final fileName =
       'lorehunterAudio_${DateTime.now().millisecondsSinceEpoch}.wav';
   final filePath = '${externalDirectory.path}$fileName';
   print(filePath);
+  final internalPath = '${internalDirectory.path}/$fileName';
 
   try {
     final files = await externalDirectory.list().toList();
@@ -60,11 +66,10 @@ Future<String> saveTTSAudio(String script, String type) async {
   try {
     await flutterTts.awaitSynthCompletion(true);
     await flutterTts.synthesizeToFile(script, fileName);
-    await Future.delayed(Durations.extralong1);
     bool doesFileExist = await File(filePath).exists();
     if (doesFileExist) {
       print("exists");
-      bytes = File(filePath).readAsBytesSync();
+      await (File(filePath)).copy(internalPath);
     } else {
       throw Exception('File not created: ' + filePath);
     }
@@ -73,57 +78,90 @@ Future<String> saveTTSAudio(String script, String type) async {
     rethrow; // Rethrow the error for handling in the calling function
   }
 
-  return filePath;
+  return internalPath;
 }
 
 /// Mixes two byte arrays representing audio data (simple averaging).
-Future<String> mixAudio(String background, String audio, String outputFilePath,
+
+Future<String> mixAudio(String audio, String background, String outputFilePath,
     {double backgroundVolume = 0.3, double taper = 1.0}) async {
-  print("mixiing audio");
-  await FFmpegKit.execute(
-          'ffmpeg -y -i $audio -i $background -filter_complex "[1:a]volume=$backgroundVolume[a1];[0:a][a1]amix=inputs=2:duration=first:dropout_transition=2[out]" -map "[out]" $outputFilePath')
-      .then((session) async {
+  // Construct the arguments list
+  final arguments = [
+    '-y',
+    '-i',
+    audio,
+    '-i',
+    background,
+    '-filter_complex',
+    '[1:a]volume=$backgroundVolume[a1];[0:a][a1]amix=inputs=2:duration=first:dropout_transition=2[out]',
+    '-map',
+    '[out]',
+    outputFilePath
+  ];
+
+  print('Executing command with arguments: $arguments');
+
+  // Execute the command with arguments
+  await FFmpegKit.executeWithArguments(arguments).then((session) async {
     final returnCode = await session.getReturnCode();
+    final output = await session.getOutput();
+    final failStackTrace = await session.getFailStackTrace();
 
     if (ReturnCode.isSuccess(returnCode)) {
-      print("success");
-      // SUCCESS
+      print("Audio mixing successful.");
     } else if (ReturnCode.isCancel(returnCode)) {
-      print('cancel');
+      print('Audio mixing was canceled.');
     } else {
-      print('error mixing audio');
+      print('Audio mixing failed.');
+      print('FFmpeg output: $output');
+      print('FFmpeg error: $failStackTrace');
     }
   });
+
   return outputFilePath;
 }
 
-Future<String> concatenateAudio(List<String> audioList, String outputFilePath,
+Future<String> concatenateAudio(List<String> audioList, int outputCount,
     {int pause = 1}) async {
-  // Create the input file list for ffmpeg
-  final inputFiles = audioList.join('|');
+  // Generate input files arguments
+  final inputFiles = audioList.expand((audio) => ['-i', audio]).toList();
+  final outputFile = await getApplicationDocumentsDirectory();
+  final outputFilePath = "${outputFile.path}/lorehunterSection$outputCount.wav";
 
-  // Create the ffmpeg command with pause between audios
-  final command =
-      'ffmpeg -i "concat:$inputFiles|${List.filled(audioList.length - 1, "pause=$pause:c:a").join('|')}"\' -acodec copy $outputFilePath';
+  // Generate the concat filter with pause
+  final filters = List.generate(audioList.length, (index) => '[$index:a]');
+  final concatFilter =
+      filters.join('') + 'concat=n=${audioList.length}:v=0:a=1[out]';
 
-  // Execute the ffmpeg command
-  final session = await FFmpegKit.executeAsync(command);
+  // Construct the arguments list
+  final arguments = [
+    '-y',
+    ...inputFiles,
+    '-filter_complex',
+    concatFilter,
+    '-map',
+    '[out]',
+    outputFilePath
+  ];
 
-  final returnCode = await session.getReturnCode();
-  final output = await session.getOutput();
-  final failStackTrace = await session.getFailStackTrace();
+  print('Executing command with arguments: $arguments');
 
-  // Handle the result
-  if (ReturnCode.isSuccess(returnCode)) {
-    print('Audio concatenation successful.');
-    return outputFilePath;
-  } else {
-    // Handle error, e.g., log error message, throw an exception
-    print('Audio concatenation failed.');
-    print('FFmpeg output: $output');
-    print('FFmpeg error: $failStackTrace');
-    return '';
-  }
+  // Execute the command with arguments
+  await FFmpegKit.executeWithArguments(arguments).then((session) async {
+    final returnCode = await session.getReturnCode();
+    final output = await session.getOutput();
+    final failStackTrace = await session.getFailStackTrace();
+
+    if (ReturnCode.isSuccess(returnCode)) {
+      print('Audio concatenation successful.');
+    } else {
+      print('Audio concatenation failed.');
+      print('FFmpeg output: $output');
+      print('FFmpeg error: $failStackTrace');
+    }
+  });
+
+  return outputFilePath;
 }
 
 Uint8List _adjustVolume(Uint8List audioBytes, double volumeRatio) {
@@ -148,17 +186,57 @@ Future<void> writeToFile(ByteData data, String path) async {
   print("saved assets");
 }
 
-Future<String> savePlaceAudio(List<Section> sections, String filename) async {
-  String headerFile;
-  String bodyFile;
-  var headerBackground = await rootBundle.load("assets/music/music_header.mp3");
-  var bodyBackground = await rootBundle.load("assets/music/music_body.mp3");
+Future<String> addSilence(String inputFilePath, String outputFilePath,
+    {double silenceDuration = 1}) async {
+  // Construct the arguments list for FFmpeg
+  final arguments = [
+    '-y',
+    '-i',
+    inputFilePath,
+    '-filter_complex',
+    '[0:a]asetpts=PTS-STARTPTS[s];'
+        'aevalsrc=0:d=$silenceDuration[s1];'
+        'aevalsrc=0:d=$silenceDuration[s2];'
+        '[s1][s][s2]concat=n=3:v=0:a=1[out]',
+    '-map',
+    '[out]',
+    outputFilePath
+  ];
 
+  print('Executing command with arguments: $arguments');
+
+  // Execute the command with arguments
+  final session = await FFmpegKit.executeWithArguments(arguments);
+
+  // Handle session result
+  final returnCode = await session.getReturnCode();
+  final output = await session.getOutput();
+  final failStackTrace = await session.getFailStackTrace();
+
+  if (ReturnCode.isSuccess(returnCode)) {
+    print('Silence addition successful.');
+    return outputFilePath;
+  } else {
+    print('Silence addition failed.');
+    print('FFmpeg output: $output');
+    print('FFmpeg error: $failStackTrace');
+    return '';
+  }
+}
+
+Future<String> savePlaceAudio(List<Section> sections, String filename) async {
   final file = await getApplicationDocumentsDirectory();
   final filePath = file.path + "/$filename.wav";
   if (await File(filePath).exists()) {
+    print("audio file already exists");
     return filePath;
   }
+  String headerFile;
+  String bodyFile;
+  var headerBackground = await rootBundle.load("assets/music/music_header.wav");
+  var bodyBackground = await rootBundle.load("assets/music/music_body.mp3");
+  final externalDirectory = "/storage/emulated/0/Music/";
+
   await writeToFile(headerBackground, file.path + "/headerBG.wav");
   await writeToFile(bodyBackground, file.path + "/bodyBG.wav");
 
@@ -168,25 +246,26 @@ Future<String> savePlaceAudio(List<Section> sections, String filename) async {
     print("Count : $count");
     count += 1;
     headerFile = await saveTTSAudio(section.header, "header");
+    headerFile =
+        await addSilence(headerFile, file.path + '/silencedHeader.wav');
 
-    print("saved header");
     headerFile = await mixAudio(
-        headerFile, file.path + "/headerBG.wav", file.path + '/headerBG.wav');
-    print("mixed header audio");
+        headerFile, file.path + "/headerBG.wav", file.path + '/header.wav',
+        backgroundVolume: 0.7);
+    print("HeaderFile:" + headerFile);
 
     bodyFile = await saveTTSAudio(section.tourAudio, "body");
-    print("saved body");
-
-    bodyFile = await mixAudio(
-        bodyFile, file.path + "/bodyBG.wav", file.path + '/bodyBG.wav');
-    print("mixed body audio");
 
     if (audio == null) {
-      audio = await concatenateAudio([headerFile, bodyFile], filePath);
+      audio = await concatenateAudio([headerFile, bodyFile], count);
     } else {
-      audio = await concatenateAudio([audio, headerFile, bodyFile], filePath);
+      audio = await concatenateAudio([audio, headerFile, bodyFile], count);
     }
   }
+  var finalAudio = await mixAudio(
+      audio!, file.path + "/bodyBG.wav", file.path + '/finalAudio.wav',
+      backgroundVolume: 0.1);
 
-  return filename;
+  await File(finalAudio).copy("$externalDirectory$filename.wav");
+  return "$externalDirectory$filename.wav";
 }
